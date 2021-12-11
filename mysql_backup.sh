8006fe6cdf6b95ca3101fe6fd3069462bd5c4941
Monty <ryan.g.montgomery@gmail.com>
2021-12-11 13:28:03 -0600

    Update mysql_backup.sh

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -8,50 +8,38 @@
 #CRON:
   # Example cron job for daily db backup at midnight
   # min  hr mday month wday command
-  # 0    0  *    *     *    /Users/[your user name]/scripts/mysql_backup.sh
+  # 0    0  *    *     *    /<script_path>/mysql_backup.sh -c <conf_file_path>/<name>.conf
 
 #RESTORE FROM BACKUP
 #  zcat [backupfile.sql.gz] | mysql -u [uname] -p[pass] [dbname]
 
 ################################################################################
-## Config SETTINGS
-################################################################################
-
-# Directory to store the backup files
-BACKUP_DIR=/var/nfs/backups/mysql_backups/wordpress_db/
-
-# MYSQL Parameters
-MYSQL_UNAME=root
-MYSQL_PWORD=qOskT1&%lOo0
-
-#Server hosting the database(s)
-MYSQL_SERVER=192.168.86.55
-MYSQL_PORT=8003
-
-# Don't backup databases with these names 
-# Example: starts with mysql (^mysql) or ends with _schema (_schema$)
-IGNORE_DB="(^mysql|_schema$)"
-
-# include mysql and mysqldump binaries for cron bash user
-PATH=$PATH:/usr/bin/mysql
-
-# Number of days to keep backups
-KEEP_BACKUPS_FOR=30 #days
-
-# YYYY-MM-DD
-TIMESTAMP=$(date +%F)
-
-################################################################################
 ## Functions
 ################################################################################
 
-function delete_old_backups()
-{
-  echo "Deleting $BACKUP_DIR/*.sql.gz older than $KEEP_BACKUPS_FOR days"
-  find $BACKUP_DIR -type f -name "*.sql.gz" -mtime +$KEEP_BACKUPS_FOR -exec rm {} \;
+ImportGlobalFunctions () {
+############################################################################
+### ImportGlobalFunctions - Purpose is to import needed functions
+############################################################################
+#Define script execution directory for Importing Global Functions
+script_dir=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
+DebugLogging "Script directory is ${script_dir}"
+
+#Importing functions
+for func_file in ${script_dir}/funcs/*.func; do
+	source ${func_file}
+done
+
 }
 
-function mysql_login() {
+DeleteOldBackups () {
+  echo "Deleting $BACKUP_DIR/*.sql.gz older than $KEEP_BACKUPS_FOR days"
+  find $BACKUP_DIR -type f -name "*.sql.gz" -mtime +$KEEP_BACKUPS_FOR -exec rm {} \;
+  CaptureExitCode
+  VerifyExitCode
+}
+
+MysqlLogin() {
   local mysql_login="-u $MYSQL_UNAME" 
   if [ -n "$MYSQL_PWORD" ]; then
     local mysql_login+=" -p$MYSQL_PWORD" 
@@ -59,12 +47,12 @@ function mysql_login() {
   echo $mysql_login
 }
 
-function database_list() {
+DatabaseList() {
   local show_databases_sql="SHOW DATABASES WHERE \`Database\` NOT REGEXP '$IGNORE_DB'"
   echo $(mysql $(mysql_login) -e "$show_databases_sql"|awk -F " " '{if (NR!=1) print $1}')
 }
 
-function echo_status(){
+EchoStatus() {
   printf '\r'; 
   printf ' %0.s' {0..100} 
   printf '\r'; 
@@ -98,6 +86,9 @@ function hr(){
 ###########################################################
 ### Main execution area
 ###########################################################
+ImportGlobalFunctions
+
+
 delete_old_backups
 hr
 backup_databases
