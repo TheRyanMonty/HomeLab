efda9d4a8478eaab2e2c93504a099bea5d7a0b59
Monty <ryan.g.montgomery@gmail.com>
2022-07-26 16:37:11 -0500

    Update post_vm_build.sh

--- post_vm_build.sh
+++ post_vm_build.sh
@@ -8,7 +8,7 @@
 #Set timezone:
 sudo timedatectl set-timezone America/Chicago
 #Install qemu-guest-agent for VMs:
-sudo apt install qemu-guest-agent
+sudo apt install qemu-guest-agent nfs-common
 #Setup vi as shell browser:
 echo "set -o vi" >> ~/.bashrc; sudo echo "set -o vi" >> ~/.bashrc
 #update and upgrade
