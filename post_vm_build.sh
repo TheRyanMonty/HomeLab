39b5bd6ef39794aa52ba3508f18d41247dc01778
Monty <ryan.g.montgomery@gmail.com>
2026-02-21 10:07:20 -0600

    Configure system settings for K3S nodes
    Added system configurations for K3S nodes to enhance inotify limits and enable IP forwarding.

--- post_vm_build.sh
+++ post_vm_build.sh
@@ -18,4 +18,21 @@ sudo echo "export EDITOR='vim'" >> /root/.bashrc
 #update and upgrade
 sudo apt update; sudo apt dist-upgrade -y
 
+##Added for K3S nodes, but shouldn't hurt any other VM
+echo "###################################################################
+# Kubernetes & Longhorn specific overrides
+# Fixes: failed to create fsnotify watcher: too many open files
+###################################################################
+
+# Increase the number of inotify user instances
+# Default is often 128; 1024 is recommended for clusters with many volumes
+fs.inotify.max_user_instances=1024
+
+# Increase the number of inotify user watches
+# Default is 8192; 1048576 is standard for Longhorn/distributed storage
+fs.inotify.max_user_watches=1048576
+
+# Ensure IP forwarding is enabled for K3s networking (Flannel/Calico)
+net.ipv4.ip_forward=1">>/etc/sysctl.conf
+
 exit 0
